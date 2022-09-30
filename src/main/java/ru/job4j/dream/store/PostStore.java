a6d11c013850a70e2bdf44469ffe2b30570e76ef
new_user <nrustgen@gmail.com>
2022-09-29 22:08:20 -0700

    4. Thymeleaf, Cycles. [#504841]

---
+++ src/main/java/ru/job4j/dream/store/PostStore.java
@@ -0,0 +1,34 @@
+package ru.job4j.dream.store;
+
+import org.springframework.format.annotation.DateTimeFormat;
+import ru.job4j.dream.model.Post;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Collection;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class PostStore {
+
+    private static final PostStore INST = new PostStore();
+
+    private final Map<Integer, Post> posts = new ConcurrentHashMap<>();
+
+    private PostStore() {
+        posts.put(1, new Post(1, "Junior Java Job",
+                "Soft develop", LocalDateTime.now()));
+        posts.put(2, new Post(2, "Middle Java Job",
+                "Strong develop", LocalDateTime.now()));
+        posts.put(3, new Post(3, "Senior Java Job",
+                "Hard develop", LocalDateTime.now()));
+    }
+
+    public static PostStore instOf() {
+        return INST;
+    }
+
+    public Collection<Post> findAll() {
+        return posts.values();
+    }
+}
