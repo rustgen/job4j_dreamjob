3cd931a72a0de1367ac3936abbd334d749fa6fba
new_user <nrustgen@gmail.com>
2022-09-30 19:31:01 -0700

    7. HTML form. Creating a vacancy. [#504844]

--- src/main/java/ru/job4j/dream/controller/PostController.java
+++ src/main/java/ru/job4j/dream/controller/PostController.java
@@ -1,18 +1,22 @@
 package ru.job4j.dream.controller;
 
+import ru.job4j.dream.model.Post;
 import ru.job4j.dream.store.PostStore;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.time.LocalDateTime;
 
 @Controller
 public class PostController {
 
     private final PostStore store = PostStore.instOf();
 
-    @GetMapping("/posts")
+    @GetMapping("/formAddPost")
     public String posts(Model model) {
-        model.addAttribute("posts", store.findAll());
-        return "posts";
+        model.addAttribute("post", new Post(0, "Junior Java Job",
+                "Soft develop", LocalDateTime.now()));
+        return "addPost";
     }
 }
