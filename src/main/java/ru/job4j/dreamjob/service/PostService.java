1c5bfd44ff2080be72cf454da908b577c2931c54
new_user <nrustgen@gmail.com>
2022-10-03 15:06:38 -0700

    1. Layered architecture. [#504851]

---
+++ src/main/java/ru/job4j/dreamjob/service/PostService.java
@@ -0,0 +1,37 @@
+package ru.job4j.dreamjob.service;
+
+import ru.job4j.dreamjob.model.Post;
+import ru.job4j.dreamjob.store.PostStore;
+
+import java.util.Collection;
+
+public class PostService {
+
+    private static final PostService INST = new PostService();
+
+    private final PostStore store = PostStore.instOf();
+
+    private PostService() {
+
+    }
+
+    private static PostService insOf() {
+        return INST;
+    }
+
+    public Collection<Post> findAll() {
+        return store.findAll();
+    }
+
+    public Post add(Post post) {
+        return store.add(post);
+    }
+
+    public Post findById(int id) {
+        return store.findById(id);
+    }
+
+    public void update(Post post) {
+        store.update(post);
+    }
+}
